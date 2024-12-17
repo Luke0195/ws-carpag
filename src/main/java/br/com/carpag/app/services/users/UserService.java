80b0a53d1bc71d658eccb3baaa0b1041531cf890
Lucas Santos <lucas201273@gmail.com>
2024-12-16 19:21:30 -0500

    feat: ensure UserService returns an exception when user email is already exists.

--- src/main/java/br/com/carpag/app/services/users/UserService.java
+++ src/main/java/br/com/carpag/app/services/users/UserService.java
@@ -2,10 +2,13 @@ package br.com.carpag.app.services.users;
 
 import br.com.carpag.app.dtos.request.UserRequestDto;
 import br.com.carpag.app.dtos.response.UserResponseDto;
+import br.com.carpag.app.models.User;
 import br.com.carpag.app.repositories.UserRepository;
 import br.com.carpag.app.services.users.usecases.AddUserUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -16,6 +19,10 @@ public class UserService implements AddUserUseCase {
 
     @Override
     public UserResponseDto addUser(UserRequestDto userRequestDto) {
-        return null;
+      Optional<User> findUserByEmail = userRepository.findByEmail(userRequestDto.email());
+      if(findUserByEmail.isPresent()) throw new RuntimeException("This email is already taken");
+      return null;
+
     }
+
 }
