e4810f330ff5a461ad770ee36237123212e08655
Lucas Santos <lucas201273@gmail.com>
2024-12-16 19:21:36 -0500

    test: ensure UserService returns an exception when user email is already exists.

--- src/test/java/br/com/carpag/app/services/users/UserServiceTest.java
+++ src/test/java/br/com/carpag/app/services/users/UserServiceTest.java
@@ -2,15 +2,19 @@ package br.com.carpag.app.services.users;
 
 import br.com.carpag.app.dtos.request.UserRequestDto;
 import br.com.carpag.app.factories.UserFactory;
+import br.com.carpag.app.models.User;
 import br.com.carpag.app.repositories.UserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("dev")
 class UserServiceTest {
@@ -31,8 +35,18 @@ class UserServiceTest {
     @BeforeEach
     void setup(){
         setupValues();
-
     }
+
+    @DisplayName("addUserUseCase should returns an exception when user e-mail already exists")
+    @Test
+    void addUserUseCaseShouldReturnsAnExceptionWhenUserEmailAlreadyExists(){
+        Mockito.when(userRepository.findByEmail(Mockito.anyString())).thenThrow(RuntimeException.class);
+        UserRequestDto userRequestDto = new UserRequestDto("any_name", "valid_email@mail.com");
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            userService.addUser(userRequestDto);
+        });
+    }
+
 
 
 }
